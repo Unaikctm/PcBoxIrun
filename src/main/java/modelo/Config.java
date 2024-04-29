6f984b16a7db42c01e85a4ece086d1fa2e194227
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-29 13:48:30 +0200

    Config mio

--- src/main/java/modelo/Config.java
+++ src/main/java/modelo/Config.java
@@ -3,8 +3,8 @@ package modelo;
 public interface Config {
 
 	public static final String BBDD = "pcbox";
-	public static final String HOST = "192.168.10.20:33060";
+	public static final String HOST = "192.168.1.20:33060";
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "pirineus";
+	public static final String PASSWORD = "mysql";
 
 }
